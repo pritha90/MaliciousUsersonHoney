6dfca2ad489af7ffea01f0d792645e44fdecbdf6
Pritha <pritha.narayanappa@gmail.com>
2016-11-15 19:42:24 -0800

    user related

--- create_new_accounts_on_Honey.py
+++ create_new_accounts_on_Honey.py
@@ -7,6 +7,9 @@ import json
 
 class HoneyUserCreator:
         url = None
+	id = None
+	createdTime = None
+	cookieJar = None
         def __init__(self, url = None):
                 self.url = "https://d.joinhoney.com/extusers"
 	def getNewEmailId(self):
@@ -15,16 +18,22 @@ class HoneyUserCreator:
 		data = {"email" : email , "password": "p1234"}
 		#return json.dumps(data, ensure_ascii=False) 
 		return "email="+email+"&password=p1234"
-        def doPost(self):
+        def createUser(self):
 		postData = self.getNewEmailId()
 		print postData
 		headers = {'content-type' : 'application/x-www-form-urlencoded'}
                 infoResponse = requests.post(self.url, data=postData, headers=headers)
-		print infoResponse.text
-		print infoResponse.cookies['honey.user']
+		userdata = json.loads(infoResponse.text)
+		self.id = userData["id"]
+		self.createdTime = userData["created"]
+		self.cookieJar = infoResponse.cookies
+		for cookie in infoResponse.cookies:
+			print cookie.expires
+			print cookie
                 return infoResponse
-
+	def placeOrder(self, response):
+		
 
 if __name__ == "__main__":
 	user = HoneyUserCreator()
-	print user.doPost().text
+	print user.createUser().text
