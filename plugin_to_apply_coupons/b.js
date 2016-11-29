04162fd9fd3ba61edfecb2d09d30c4a20f917d65
Pritha <pritha.narayanappa@gmail.com>
2016-11-28 17:06:03 -0800

    bestbuy action

--- plugin_to_apply_coupons/b.js
+++ plugin_to_apply_coupons/b.js
@@ -1,1 +1,4 @@
 //THe js file to perform background tasks
+//Actions for bestbuy
+document.getElementsByClassName("form-control")[2].value="ppppp"
+document.getElementsByClassName("btn btn-large btn-primary emphasized-copy")[1].click()
