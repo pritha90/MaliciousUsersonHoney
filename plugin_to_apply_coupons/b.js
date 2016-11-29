67108c6357b8d5c0495b69fadf9667e761d678d0
Pritha <pritha.narayanappa@gmail.com>
2016-11-28 17:19:35 -0800

    Apply code on Bestbuy on first time load of page

--- plugin_to_apply_coupons/b.js
+++ plugin_to_apply_coupons/b.js
@@ -1,4 +1,1 @@
 //THe js file to perform background tasks
-//Actions for bestbuy
-document.getElementsByClassName("form-control")[2].value="ppppp"
-document.getElementsByClassName("btn btn-large btn-primary emphasized-copy")[1].click()
